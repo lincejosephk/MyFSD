2f85601234933dc126aacf219d0759cc0a3d855d
Lince Joseph Kadanthottu <lince.joseph@cognizant.com>
2018-05-04 17:37:43 +0530

    My Project manager changes

--- ProjectManagerUI/karma.conf.js
+++ ProjectManagerUI/karma.conf.js
@@ -10,7 +10,8 @@ module.exports = function (config) {
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
       require('karma-coverage-istanbul-reporter'),
-      require('@angular/cli/plugins/karma')
+      require('@angular/cli/plugins/karma'),
+      require('karma-junit-reporter')
     ],
     client:{
       clearContext: false // leave Jasmine Spec Runner output visible in browser
@@ -22,12 +23,27 @@ module.exports = function (config) {
     angularCli: {
       environment: 'dev'
     },
-    reporters: ['progress', 'kjhtml'],
+    junitReporter: {
+      outputDir: './Report', 
+      outputFile: undefined, 
+      suite: '', 
+      useBrowserName: true,  
+      nameFormatter: undefined, 
+      classNameFormatter: undefined, 
+      properties: {} 
+    },
+    reporters: ['progress', 'junit'],
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome'],
-    singleRun: false
+    browsers: ['HeadlessChrome'],
+        customLaunchers: {
+            HeadlessChrome: {
+                base: 'ChromeHeadless',
+                flags: ['--no-sandbox']
+            }
+        },
+    singleRun: true
   });
 };
